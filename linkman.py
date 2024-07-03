134d8a55100497dbfcc728542a5d3b6027068149
7pk5 <122971824+7pk5@users.noreply.github.com>
2024-07-04 02:39:05 +0530

    Update linkman.py

--- linkman.py
+++ linkman.py
@@ -9,7 +9,7 @@ from git import Repo
 
 # File path for the local Excel file
 file_path = 'link_data.xlsx'
-repo_path = 'https://github.com/7pk5/linkman/blob/main/link_data.xlsx'
+repo_path = 'https://github.com/7pk5/linkman/main/link_data.xlsx'
 
 # Function to check if the file exists
 def file_exists(file_path):
